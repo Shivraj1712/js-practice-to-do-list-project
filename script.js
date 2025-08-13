470edb240dbd2484bea0f3743252d8f1e41b44ea
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2025-08-13 15:13:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function addTask(){
 }
 
 listContainer.addEventListener("click",function(e){
-    if( e.target.tagName ==='li' ){
+    if( e.target.tagName ==='LI' ){
         e.target.classList.toggle("checked");
         saveData();
-    }else if( e.target.tagName==-'span' ){
+    }else if( e.target.tagName==='SPAN' ){
         e.target.parentElement.remove();
         saveData();
     }
@@ -32,4 +32,5 @@ function saveData(){
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
 showTask();
