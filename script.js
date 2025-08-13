29bb4abd1b00db22b532c1dadbba88f040bd0ab2
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2025-08-13 10:31:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must write something in the input box");
+    }else{
+        let li = document.createElement("li");
+        li.innerText = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerText='\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value='';
+    saveData();
+}
+
+listContainer.addEventListener("click",function(e){
+    if( e.target.tagName ==='li' ){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if( e.target.tagName==-'span' ){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
